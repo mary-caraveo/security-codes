d911e601710577ceb9f6a7b265b3a8b239b7ed55
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-11-14 16:47:46 -0500

    feat: independent states with useState

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,22 +1,33 @@
 import React, { useState, useEffect } from 'react';
 
+const SECURITY_CODE = 'paradigma';
+
 const UseState = ({ name }) => {
+  const [value, setValue] = useState('');
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  console.log(value)
 
   useEffect(() => {
     console.log("Empezando un efecto")
 
     if (!!loading) {
+      setError(false);
       setTimeout(() => {
         console.log("Realizando la validación")
+
+        if (value !== SECURITY_CODE) {
+          setError(true);
+        }
         setLoading(false);
+
         console.log("Terminando la validación")
       }, 3000)
     }
 
     console.log("Terminando el efecto")
-  }, [loading]);
+  }, [loading, value]);
 
   return (
     <div>
@@ -31,7 +42,13 @@ const UseState = ({ name }) => {
         <p>Cargando...</p>
       )}
 
-      <input placeholder="Código de Seguridad" />
+      <input
+        placeholder="Código de Seguridad"
+        value={value}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+      />
       <button onClick={() => setLoading(true)}>Comprobar</button>
     </div>
   );
