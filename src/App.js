a16460ef6432d795904208da2bebdaea064e70ae
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-11-22 20:52:38 -0500

    feat: add styles to the application

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <UseState name="Use State" />
-      <UseReducer name="Use reducer" />
+    <div className='App'>
+      <h1>React.js: Manejo Profesional del Estado</h1>
+      <div className="content-app">
+        <UseState name="Use State" />
+        <UseReducer name="Use reducer" />
+      </div>
     </div>
   );
 }
